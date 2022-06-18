786379cdba9f1d667cb6e8962377df960fcb6095
luk.lu <luk.lu@faronear.org>
2022-06-18 15:41:23 +0800

    npm run git-import 来导入应用代码

---
+++ tools/git-import.sh
@@ -0,0 +1,7 @@
+echo "=== Enter a << org/repo >> to add as submodule, or << leave blank >> for no change):" 
+read -p ">>> " SubRepo
+if [ $SubRepo ]
+then
+  git submodule add https://git.faronear.org/$SubRepo src ## 注意，不能用 ln -s 来链接外部项目到 src! vue-cli 不会对链接目录进行 babel 转译，导致 npm run serve 无法识别 ES6 写法，无法编译通过
+  cd src && npm run boot && cd ..
+fi
