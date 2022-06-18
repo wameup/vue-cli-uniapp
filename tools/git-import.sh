370297097f85dc8d252fa9ac14ba8c416e6884d3
luk.lu <luk.lu@faronear.org>
2022-06-18 15:58:11 +0800

    u

--- tools/git-import.sh
+++ tools/git-import.sh
@@ -4,4 +4,5 @@ if [ $SubRepo ]
 then
   git submodule add https://git.faronear.org/$SubRepo src ## 注意，不能用 ln -s 来链接外部项目到 src! vue-cli 不会对链接目录进行 babel 转译，导致 npm run serve 无法识别 ES6 写法，无法编译通过
   cd src && npm run boot && cd ..
+  npm run serve
 fi
